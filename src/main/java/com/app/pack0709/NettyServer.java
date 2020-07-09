b28d56842a6d4c68e844e0b52b5ca577acc61fd3
makoto4423 <52313862+makoto4423@users.noreply.github.com>
2020-07-09 17:34:09 +0800

    netty

--- src/main/java/com/app/pack0709/NettyServer.java
+++ src/main/java/com/app/pack0709/NettyServer.java
@@ -3,16 +3,23 @@ package com.app.pack0709;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoop;
+import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.concurrent.DefaultThreadFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
+
+import java.util.logging.Logger;
 
 public class NettyServer {
-
+    // private static final InternalLogger logger = InternalLoggerFactory.getInstance(Bootstrap.class);
     public static void  main(String[] args){
-        EventLoopGroup  bossGroup = new DefaultEventLoop();
-        EventLoopGroup workerGroup = new DefaultEventLoop();
+        final Logger logger = Logger.getLogger(NettyServer.class.getName());
+        final EventLoopGroup  bossGroup = new NioEventLoopGroup();
+        final EventLoopGroup workerGroup = new NioEventLoopGroup();
         try{
             ServerBootstrap b = new ServerBootstrap();
             b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
@@ -23,12 +30,16 @@ public class NettyServer {
                             p.addLast(new LoggingHandler(LogLevel.INFO));
                         }
                     });
-            b.bind(18080).sync();
+            ChannelFuture f = b.bind(18080).sync();
+            f.channel().closeFuture().addListener(new ChannelFutureListener() {
+                public void operationComplete(ChannelFuture future) {
+                    logger.warning(future.channel().toString()+"close");
+                    bossGroup.shutdownGracefully();
+                    workerGroup.shutdownGracefully();
+                }
+            });
         } catch (InterruptedException e) {
             e.printStackTrace();
-        } finally {
-            bossGroup.shutdownGracefully();
-            workerGroup.shutdownGracefully();
         }
 
     }
